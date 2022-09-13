245b1ed68786438abceb2b7ee56c10f0fe9fa5fd
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-09-13 18:49:40 +0300

    start the project

---
+++ main.py
@@ -0,0 +1,16 @@
+def get_data(url):
+    headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    }
+
+
+def main():
+    get_data('https://www.kijiji.ca/b-apartments-condos/city-of-toronto/c37l1700273')
+
+
+if __name__ == '__main__':
+    main()
