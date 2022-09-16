40b28fa46a7d218e0e178b80dc24884a87b3bdb8
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-09-16 10:25:31 +0300

    make parsers

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
-
+from json import JSONDecoder
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 
 
 def get_data_with_selenium(url):
@@ -16,9 +17,35 @@ def get_data_with_selenium(url):
             options=options,
          )
         driver.get(url=url)
-        element = driver.find_elements(By.XPATH, '/html/body/div[3]/div[3]/div[3]/div[3]/main/div[2]')
-        for elements in element:
-            print(elements.text)
+ 
+
+        date = driver.find_elements(By.XPATH, "//span[@class='date-posted']")
+        for dates in date:
+            print(dates.text)
+
+        location = driver.find_elements(By.XPATH, "//div[@class='location']")
+        for obj in location:
+            locations = obj.find_element(By.TAG_NAME, 'span')
+            print(locations.text)
+
+        description = driver.find_elements(By.XPATH, "//div[@class='description']")
+        for descriptions in description:
+            print(descriptions.text)
+
+        title = driver.find_elements(By.XPATH, "//a[contains(@class,'title')]")
+        for titles in title:
+            print(titles.text)
+
+        price = driver.find_elements(By.XPATH, "//div[contains(@class,'price')]")
+        for prices in price:
+            print(prices.text)
+
+        images = driver.find_elements(By.XPATH, '//img')
+        for img in images:
+            if "https://media.kijiji.ca/api/v1/ca-prod-dealer-ads/images/" in img.get_attribute('src'):
+                print(img.get_attribute('src'))
+
+
 
         time.sleep(5)
 
@@ -27,7 +54,6 @@ def get_data_with_selenium(url):
     finally:
          driver.close()
          driver.quit()
-
 
 get_data_with_selenium('https://www.kijiji.ca/b-apartments-condos/city-of-toronto/c37l1700273')
 
